f8681b1fac5c13a717f1939d65838b8c552b8f92
Rossano Bavaresco <rossanorb@gmail.com>
2020-12-20 19:33:55 -0300

    comentando log

--- src/routes.php
+++ src/routes.php
@@ -9,7 +9,7 @@ $container['db'];
 
 $app->get('/', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    // $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
