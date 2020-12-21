b61237ff6ce5c6af80607df75edee02c3845d6b9
Rossano Bavaresco <rossanorb@gmail.com>
2020-12-21 12:26:54 -0300

    Testando login

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Http\Environment;
+
+class UserTest extends BaseTestCase
+{
+    public function testLogin()
+    {
+        $response = $this->runApp('POST', '/login', [
+            'username' => 'admin',
+            'password' => '@#$RF@!718'
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('token', (string)$response->getBody());
+
+    }
+
+}
