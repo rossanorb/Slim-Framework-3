280045951b9db0c8b4a1376656ab1cd18c202ada
Rossano Bavaresco <rossanorb@gmail.com>
2020-12-21 11:22:35 -0300

    Restrigindo acesso endpoints users e breeds

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,14 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+use Tuupola\Middleware\JwtAuthentication;
+
+
+$app->add(new JwtAuthentication([
+    'regexp' => '/(.*)/',
+    'header' => 'Authorization',
+    "path" => ["/users", "/breeds"],
+    'realm' => 'Protected',
+    'secret' => $container['settings']['secretKey']
+]));
