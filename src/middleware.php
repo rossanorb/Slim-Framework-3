10ae498465e5b3abbbc33a1e95daabca3f8ab2cd
Rossano Bavaresco <rossanorb@gmail.com>
2020-12-21 11:24:17 -0300

    liberando requisicoes XMLHttpRequest (CORS)

--- src/middleware.php
+++ src/middleware.php
@@ -13,3 +13,11 @@ $app->add(new JwtAuthentication([
     'realm' => 'Protected',
     'secret' => $container['settings']['secretKey']
 ]));
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, Origin, Accept')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
